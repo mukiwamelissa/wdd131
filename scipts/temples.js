a5626266b87f121c081ff8745ddeedebe351e835
mukiwamelissa <mukiwamelissa@gmail.com>
2026-07-15 22:32:32 +0200

    commit

---
+++ scipts/temples.js
@@ -0,0 +1,17 @@
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+
+    if (navigation.classList.contains("open")) {
+        menuButton.textContent = "✖";
+    } else {
+        menuButton.textContent = "☰";
+    }
+});
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
+`Last Modified: ${document.lastModified}`;
